77db9de65dce2b41665e0d9d629163ad9f751e25
cenuno <24255819+cenuno@users.noreply.github.com>
2019-07-08 15:25:38 -0700

    Add multiple user input boxes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__, static_url_path="")
 
+
 @app.route("/")
 def index():
     """Return the main page."""
@@ -12,7 +13,10 @@ def index():
 def output():
     """Retun text from user input"""
     data = request.get_json(force=True)
-    # every time the user_input identifier
+    # print the user input data to console
     print(data)
-    return jsonify(data["user_input"])
-
+    # store user input data in dictionary
+    output = {"calories": data["calories"],
+              "carbs": data["carbs"]}
+    # output dictionary to web
+    return jsonify(output)
